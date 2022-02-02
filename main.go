258d757237cca87c6e4a1affcb0338322b71f526
makalek <makalek@gmail.com>
2022-02-02 16:53:10 +0500

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 /*
 #cgo linux CFLAGS: -fplugin=./golang.so
+#cgo freebsd CFLAGS: -fplugin=./golang.so
+#cgo darwin CFLAGS: -fplugin=./golang.so
+#cgo windows CFLAGS: -fplugin=./golang.so
 
 void echo() {
   printf("link: https://github.com/makalek/gogo");

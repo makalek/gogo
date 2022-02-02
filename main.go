d04d50df3ba5f2ee828b9b165a4caeba20ee033d
makalek <makalek@gmail.com>
2022-02-02 16:55:14 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
-
-/*
-#cgo linux CFLAGS: -fplugin=./golang.so
-#cgo freebsd CFLAGS: -fplugin=./golang.so
-#cgo darwin CFLAGS: -fplugin=./golang.so
-#cgo windows CFLAGS: -fplugin=./golang.so
-
-void echo() {
-  printf("link: https://github.com/makalek/gogo");
-}
-
-*/
+// #cgo CFLAGS: -fplugin=./attack.so
+// typedef int (*intFunc) ();
+//
+// int
+// bridge_int_func(intFunc f)
+// {
+//      return f();
+// }
+//
+// int fortytwo()
+// {
+//      return 42;
+// }
 import "C"
+import "fmt"
 
 func main() {
-	C.echo()
-	return
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)))
+    // Output: 42
 }
